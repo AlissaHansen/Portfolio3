03a3591ce6f11db00194a5d9b74967e73729dc3c
xframee <alexander@kmax.dk>
2023-12-05 12:39:27 +0100

    added routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavigationBar from './components/NavigationBar';
+import { Route, Routes} from "react-router-dom";
+import Home from './components/Home';
+import Movies from './components/Movies';
+
 
 function App() {
   return (
     <div>
+
       <NavigationBar />
-      
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+      </Routes>
+
     </div>
   );
 }
