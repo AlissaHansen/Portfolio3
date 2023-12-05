4fab7d4a93233c0797bebe9405fc48c025984332
xframee <alexander@kmax.dk>
2023-12-05 12:39:41 +0100

    added new pages

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -16,11 +16,9 @@ const NavigationBar = () =>
                     alt="React Bootstrap logo"
                 />
             </Navbar.Brand>
-            <Navbar.Brand href="#home">MovieDB</Navbar.Brand>
+            <Navbar.Brand href="/">MovieDB</Navbar.Brand>
             <Nav className="me-auto">
-                <Nav.Link href="#movies">Movies</Nav.Link>
-                <Nav.Link href="#actors">Actors</Nav.Link>
-                <Nav.Link href="#">Pricing</Nav.Link>
+                <Nav.Link href="/movies">Movies</Nav.Link>
             </Nav>
         </Container>
     </Navbar>;
