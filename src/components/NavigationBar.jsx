c0d23df066203ec5fce020a167757c44ce3eac26
xframee <alexander@kmax.dk>
2023-12-06 10:58:28 +0100

    added searchbar

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import MoviedbLogo from '../images/moviedblogo.svg';
 
 const NavigationBar = () =>
@@ -19,6 +23,21 @@ const NavigationBar = () =>
             <Navbar.Brand href="/">MovieDB</Navbar.Brand>
             <Nav className="me-auto">
                 <Nav.Link href="/movies">Movies</Nav.Link>
+                
+                <Form inline>
+                    <Row>
+                        <Col xs="auto" className="ml-auto">
+                            <Form.Control
+                                type="text"
+                                placeholder="Search"
+                                className=" mr-sm-2"
+                            />
+                        </Col>
+                        <Col xs="auto">
+                            <Button type="submit">Submit</Button>
+                        </Col>
+                    </Row>
+                </Form>
             </Nav>
         </Container>
     </Navbar>;
