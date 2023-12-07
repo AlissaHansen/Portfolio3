b009f79d7cfe0191da9c89c2adf2f024aaa5c46c
xframee <alexander@kmax.dk>
2023-12-07 12:29:25 +0100

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import MovieCarousel from "./MovieCarousel";
+import MovieListComponent from "./MovieListComponenet";
 
 const Home = () => 
 <div>
     <MovieCarousel />
+    <MovieListComponent title="Top rated movies" />
 </div>;
 
 export default Home;
