c4209a8450b8d4d01c31213a68f6e8f669392754
Alissa Hansen <79077747+AlissaHansen@users.noreply.github.com>
2023-12-07 17:16:53 +0100

    Update MovieInfo.jsx

--- src/components/MovieInfo.jsx
+++ src/components/MovieInfo.jsx
@@ -10,10 +10,8 @@ const MovieInfo = ({movie, index}) => (
       <Card.Img variant="top" src={movie.poster} style={{ height: '320px' }} />
       <Card.Body>
         <Card.Title>{movie.primarytitle}</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
+        <Card.Text>{movie.startYear}</Card.Text>
+        <Card.Text> Rating: {movie.averageRating}</Card.Text>
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
     </Card>
