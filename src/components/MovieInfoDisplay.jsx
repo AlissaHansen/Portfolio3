5dc84e13ff993a3fead0c446edd9017175adff1e
Alissa Hansen <79077747+AlissaHansen@users.noreply.github.com>
2023-12-13 12:21:25 +0100

    Update MovieInfoDisplay.jsx

--- src/components/MovieInfoDisplay.jsx
+++ src/components/MovieInfoDisplay.jsx
@@ -2,12 +2,32 @@ import React from "react";
 import { useEffect, useState } from "react";
 
 const MovieInfoDisplay = ({id}) => {
+const [movie, setMovie] = useState();
 
+useEffect(() => {
+    const loadMovie = () => {
+        const url = "http://localhost:5001/api/movieinfos/" + id
+
+        fetch (url)
+            .then((res) => res.json())
+            .then ((json) => setMovie(json))
+            .catch((error) => console.error("Error loading movie:", error));
+    };
+    loadMovie();
+}, [id]);
 
 return (
     <div>
-        {id}
+      {movie ? (
+        <div>
+          <h1>{movie.primarytitle}</h1>
+          <h1>hejjjjj</h1>
+          {/* Render other movie information here */}
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
-);
+  );
 }
 export default MovieInfoDisplay;
