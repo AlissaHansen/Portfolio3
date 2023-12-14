3791480a8d235585ee37231fc0b5be949697369e
xframee <alexander@kmax.dk>
2023-12-14 12:14:16 +0100

    Update WatchlistButton.jsx

--- src/components/WatchlistButton.jsx
+++ src/components/WatchlistButton.jsx
@@ -5,12 +5,18 @@ const WatchlistButton = ({movieId, userId}) => {
     const handleSubmit = (event) => {
         event.preventDefault();
 
+        const bookmarkData = {
+            MovieInfoId: movieId,
+            userId: userId,
+        };
+
         fetch("http://localhost:5001/api/moviebookmarks", {
-            method: 'POST',
+            method: "POST",
             headers: {
-                'Content-Type': 'application/json',
+                "Content-Type": "application/json",
+                "Authorization": "Bearer eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTUxMiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoieEZyYW1lZSIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IlVzZXIiLCJleHAiOjE3MDI4MTExNjJ9.aBnStd1yE9wfHl2Ysx2qwoxx3JqZRrbUdyBlfxqmYDfcpBX5uSTrf45kqK8m29e6D1uDlopaEBuj1MDdwUkMKA"
             },
-            body: JSON.stringify({ movieId, userId }),
+            body: JSON.stringify(bookmarkData),
         })
         .then(response => response.json())
         .then(data => {
