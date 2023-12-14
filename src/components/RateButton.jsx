e8f6d12d9493fbc05fd09704c7fbd82a1b224247
xframee <alexander@kmax.dk>
2023-12-14 11:33:00 +0100

    Create RateButton.jsx

---
+++ src/components/RateButton.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+
+const RateButton = () => {
+    return (
+        <div>
+            <Button className="Rate-button" variant="secondary">Rate</Button>
+        </div>
+    );
+}
+
+export default RateButton;
