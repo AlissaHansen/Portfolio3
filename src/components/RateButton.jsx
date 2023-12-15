49868b772286577113f30c6a6895554f32aebc32
Alissa Hansen <79077747+AlissaHansen@users.noreply.github.com>
2023-12-15 11:02:15 +0100

    Update RateButton.jsx

--- src/components/RateButton.jsx
+++ src/components/RateButton.jsx
@@ -1,12 +1,48 @@
-import React from "react";
-import Button from "react-bootstrap/Button";
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
+import '../Stylesheets/GeneralStylesheet.css';
 
-const RateButton = (movieId, userId, user) => {
-    return (
-        <div className="Rate-button">
-            <Button className="Rate-button" variant="secondary">Give a rating</Button>
-        </div>
-    );
+const RateButton = ({ movieId, userId, user }) => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button className="Rate-button" variant="light" onClick={handleShow}>
+        Rate
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Rate movie: </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Rating</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="1"
+                autoFocus
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Cancel
+          </Button>
+          <Button variant="success" onClick={handleClose}>
+            Rate
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
 }
 
 export default RateButton;
