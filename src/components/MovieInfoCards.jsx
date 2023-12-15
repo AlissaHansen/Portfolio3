1089375efd2d6f93d15b0cb1cd7f60d9f8285b8a
xframee <alexander@kmax.dk>
2023-12-15 09:10:03 +0100

    added pagination

--- src/components/MovieInfoCards.jsx
+++ src/components/MovieInfoCards.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { Link } from "react-router-dom";
+import '../Stylesheets/GeneralStylesheet.css';
 
 
 
@@ -10,7 +11,7 @@ const MovieInfo = ({movie, index}) => (
       <Link to={"/movieinfo/" + movie.id}>
     <Card style={{ width: '14rem'}}>
       <Card.Img variant="top" src={movie.poster} style={{ height: '320px' }} />
-      <Card.Body>
+      <Card.Body className="Card-body-fixed">
         <Card.Title>{movie.primarytitle}</Card.Title>
         <Card.Text>{movie.startYear}</Card.Text>
         <Card.Text> Rating: {movie.averageRating}&#9733;</Card.Text>
--- src/components/MovieListComponenet.jsx
+++ src/components/MovieListComponenet.jsx
@@ -1,31 +1,44 @@
 import React, { useEffect, useState } from "react";
 import MovieInfoCard from "./MovieInfoCards";
+import Button from 'react-bootstrap/Button';
+import '../Stylesheets/GeneralStylesheet.css';
 
 const MovieListComponent = ({ title, count, ranked }) => {
     const [movies, setMovies] = useState([]);
+    const [page, setPage] = useState(0);
+    const [numberOfPages, setNumberOfPages] = useState(0);
 
     useEffect(() => {
         const loadMovies = () => {
             const url = ranked
-                ? "http://localhost:5001/api/movieinfos?ranked=true&pagesize=" + count
-                : "http://localhost:5001/api/movieinfos?newest=true&pagesize=" + count   
+                ? "http://localhost:5001/api/movieinfos?ranked=true&pagesize=" + count + "&page=" + page
+                : "http://localhost:5001/api/movieinfos?newest=true&pagesize=" + count + "&page=" + page
 
-            fetch (url)
+            fetch(url)
                 .then((res) => res.json())
-                .then((json) => setMovies(json.items))
+                .then((json) => {
+                    setMovies(json.items);
+                    setNumberOfPages(json.numberOfPages);
+                })
                 .catch((error) => console.error("Error loading movies:", error));
         };
 
         loadMovies();
-    }, [count, ranked]);
+    }, [count, ranked, page]);
 
     return (
         <div>
             <h1 className="Movies-headline">{title}</h1>
             <div className="Cards-outer-div" >
-            {movies.map((movie, index) => (
-                <MovieInfoCard movie={movie} index={index} />
-            ))}
+                {movies.map((movie, index) => (
+                    <MovieInfoCard movie={movie} index={index} />
+                ))}
+            </div>
+            <div className="Paging-controls">
+                <Button variant="dark" onClick={() => setPage(prevPage => prevPage - 1)}>&lt;</Button>
+                <Button variant="dark" onClick={() => setPage(0)}>1</Button>
+                <Button variant="dark" onClick={() => setPage(5526)}>({numberOfPages})</Button>
+                <Button variant="dark" onClick={() => setPage(prevPage => prevPage + 1)}>&gt;</Button>
             </div>
         </div>
     );
