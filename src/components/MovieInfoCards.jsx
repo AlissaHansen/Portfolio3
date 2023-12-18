c327b716b0f62cb7aab39db9c33a50560ed3131c
xframee <alexander@kmax.dk>
2023-12-18 18:04:11 +0100

    Update MovieInfoCards.jsx

--- src/components/MovieInfoCards.jsx
+++ src/components/MovieInfoCards.jsx
@@ -6,19 +6,19 @@ import '../Stylesheets/GeneralStylesheet.css';
 
 
 
-const MovieInfoCards = ({movie, index}) => (
-    <div className="col-md-3 mb-4" key={index}>
-      <Link to={"/movieinfo/" + movie.id}>
-    <Card style={{ width: '14rem'}} className="mx-auto">
-      <Card.Img variant="top" src={movie.poster} style={{ height: '320px' }} />
-      <Card.Body className="Card-body-fixed">
-        <Card.Title>{movie.primarytitle}</Card.Title>
-        <Card.Text>{movie.startYear}</Card.Text>
-        <Card.Text> Rating: {movie.averageRating}&#9733;</Card.Text>
-        <Button variant="primary">See more</Button>
-      </Card.Body>
-    </Card>
+const MovieInfoCards = ({ movie, index }) => (
+  <div className="col-md-3 mb-4" key={index}>
+    <Link to={"/movieinfo/" + movie.id}>
+      <Card style={{ width: '14rem' }} className="mx-auto">
+        <Card.Img variant="top" src={movie.poster} style={{ height: '320px' }} />
+        <Card.Body className="Card-body-fixed">
+          <Card.Title>{movie.primarytitle}</Card.Title>
+          <Card.Text>{movie.startYear}</Card.Text>
+          <Card.Text> Rating: {movie.averageRating}&#9733;</Card.Text>
+          <Button variant="primary">See more</Button>
+        </Card.Body>
+      </Card>
     </Link>
-    </div>
+  </div>
 );
 export default MovieInfoCards;
