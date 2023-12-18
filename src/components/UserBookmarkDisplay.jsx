c10aedb393ae661b6f6660df64fe553279d3e95d
Alissa Hansen <79077747+AlissaHansen@users.noreply.github.com>
2023-12-18 20:13:27 +0100

    bookmark

--- src/components/UserBookmarkDisplay.jsx
+++ src/components/UserBookmarkDisplay.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import '../Stylesheets/GeneralStylesheet.css';
 
 const UserBookmarkDisplay = ({ user }) => {
     return (
-        <div>
+        <div className="Bookmark-container">
+            <h3>My watchlist</h3>
             {user && user.movieBookmarkModels &&
                 <ul>
                     {user.movieBookmarkModels.map((bookmark, index) => (
@@ -16,4 +18,4 @@ const UserBookmarkDisplay = ({ user }) => {
     );
 }
 
-export default UserBookmarkDisplay; hg
+export default UserBookmarkDisplay; 
