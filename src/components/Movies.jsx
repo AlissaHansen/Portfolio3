ebdb5b236bbbfbfcd71c0307e2d23244e7a01b0a
xframee <alexander@kmax.dk>
2023-12-18 11:58:40 +0100

    Update Movies.jsx

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from 'react-bootstrap/Button';
 import MovieListComponent from "./MovieListComponenet";
 import '../Stylesheets/GeneralStylesheet.css';
 
-const Movies = () =>
-    <div>
-        <MovieListComponent title="Featured movies" count={30}/>
-    </div>;
+const Movies = () => {
 
+    const [filter, setFilter] = useState("featured");
+
+    return (
+
+        <div>
+            <div className="Movie-filter-buttons">
+                <Button variant="secondary" onClick={() => setFilter("ranked")}>Ranked</Button>
+                <Button variant="secondary" onClick={() => setFilter("featured")}>Featured</Button>
+            </div>
+            <MovieListComponent title={filter + " movies"} count={20} filter={filter} />
+        </div>
+    );
+}
 export default Movies;
