4e46258d72edd3cd754ca7abb50b9ef797c06c49
xframee <alexander@kmax.dk>
2023-12-18 11:57:44 +0100

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import '../Stylesheets/GeneralStylesheet.css';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            MovieDB
+        </div>
+    );
+}
+
+export default Footer;
